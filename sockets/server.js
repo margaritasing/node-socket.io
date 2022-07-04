31f1d5097e15ac33cc99c9e46b5689d55cc369c2
margaritasing <libiamfreitesm@gmail.com>
2022-07-04 17:25:02 -0300

    agrego cambio en el server

--- sockets/server.js
+++ sockets/server.js
@@ -1,25 +1,32 @@
 const express = require('express');
 const  app = express();
+
 const http = require('http');
+
 const server = http.createServer(app);
+
+
+
+server.listen(3000)
+
+app.use(express.static('public'));
+
 const { Server } = require("socket.io");
 const io = new Server(server);
-
-app.get('/', (req, res)=> {
-    res.send('<h1>Hello Mundo</h1>')
-})
-
+  
+/* 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-  });
+    res.sendFile('public' + '/index.html');
+  }); */
 
   io.on('connection', (socket) => {
-    console.log('a user connected');
+    console.log('nueva conexion id: ' + socket.id);
+
+    io.on('evento', ()=>{});
+
+    io.emit('evento2', datos)
   });
 
 
-server.listen(3000, ()=> {
-    console.log("Servidor arrancado")
-})
 
 
